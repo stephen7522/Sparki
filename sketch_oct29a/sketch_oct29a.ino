1614a47ef4bc88e7adceb297331c844e7ab751fd
stephen7522 <stephen7522@gmail.com>
2016-10-29 12:02:44 -0400

    changed x*x to (x++)(X++)

--- sketch_oct29a/sketch_oct29a.ino
+++ sketch_oct29a/sketch_oct29a.ino
@@ -1,9 +1,17 @@
+#include <Sparki.h>
+int x=1;
 void setup() {
   // put your setup code here, to run once:
-
+sparki.clearLCD();
+sparki.println(x);
+sparki.updateLCD();
+delay(1000);
 }
 
 void loop() {
+  (x++)*(x++);
+sparki.println((x++)*(x++));
+sparki.updateLCD();
+delay(1000);
   // put your main code here, to run repeatedly:
-
 }
